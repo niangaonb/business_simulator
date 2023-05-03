57564b350d9c7b9ef124cc7b7fb84061d8cb0b50
niangaoa <niangaoa@hotmail.com>
2023-05-03 13:45:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# business_simulator
+
+兴游工作室第一款游戏，以Flutter为前端,遵循GNU协议。此仓库为前端程序(大饼阶段)
+
+
+
+
---
+++ android/app/src/main/kotlin/com/niangaoa/business_simulator/business_simulator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.niangaoa.business_simulator.business_simulator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
